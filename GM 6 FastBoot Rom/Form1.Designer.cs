2f7d31301702aa31de84d25d9d80c48aa2a84e3b
Barış Mızraklı <barismizrakli@outlook.com>
2019-04-16 22:27:22 +0300

    Fastboot Rom Installer

---
+++ GM 6 FastBoot Rom/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace GM_6_FastBoot_Rom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cihaz = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cihaz
+            // 
+            this.cihaz.AutoSize = true;
+            this.cihaz.Location = new System.Drawing.Point(65, 11);
+            this.cihaz.Name = "cihaz";
+            this.cihaz.Size = new System.Drawing.Size(269, 18);
+            this.cihaz.TabIndex = 1;
+            this.cihaz.Text = "Lütfen cihazı FastBoot Mode Alınız";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cihaz);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.panel1.Location = new System.Drawing.Point(12, 195);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(488, 37);
+            this.panel1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(49, 19);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(22, 115);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(275, 23);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Visible = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(20, 8);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 22);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Full Rom";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(143, 8);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(120, 22);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "Parçalı Rom";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(20, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(243, 34);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Seçimimi Uygula";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(16, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Seçim Bekleniyor...";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.checkBox2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.checkBox1);
+            this.panel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.panel2.Location = new System.Drawing.Point(14, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(318, 97);
+            this.panel2.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(34, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(222, 30);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Rom\'u Yükle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(22, 115);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(275, 23);
+            this.comboBox2.TabIndex = 12;
+            this.comboBox2.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_2);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(403, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Barış MIZRAKLI";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::GM_6_FastBoot_Rom.Properties.Resources.IMG_20180905_WA0007;
+            this.pictureBox2.Location = new System.Drawing.Point(339, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(168, 96);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.ForestGreen;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(425, 115);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 45);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Güvenli Çıkış";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(502, 244);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "General Mobile GM6 FastBoot Rom";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label cihaz;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ GM 6 FastBoot Rom/Form1.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace GM_6_FastBoot_Rom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bu program Barış MIZRAKLI tarafından GsmTurkey.net adına kodlanmıştır . Başka kaynaklarda paylaşılması yasaktır. !","Bilgilendirme");
+            MessageBox.Show("Yönergeleri takip etmeyi unutmayın !","Uyarı");
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            /////////////////////////////////////////
+
+            comboBox1.Items.Clear();
+            comboBox2.Items.Clear();
+
+            /////////////////////////////////////////
+            if (checkBox1.Checked == true)
+            {
+                comboBox2.Items.Add("Tek Sim Rom (Full)");
+                comboBox2.Items.Add("Çift Sim Rom (Full)");
+                label4.Text = "Full Rom Kurmayı Seçtiniz";
+                comboBox1.Visible = false;
+                comboBox2.Visible = true;
+            }
+            if (checkBox2.Checked == true)
+            {
+                comboBox1.Items.Add("Stock Boot Yükle");
+                comboBox1.Items.Add("Stock Recovery Yükle");
+                comboBox1.Items.Add("Stock Logo");
+                label4.Text = "Parçalı Rom Kurmayı Seçtiniz";
+                comboBox1.Visible = true;
+                comboBox2.Visible = false;
+
+
+            }
+            if (checkBox1.Checked == true && checkBox2.Checked == true)
+                {
+                label4.Text = "Lütfen sadece bir adet seçim yapınız !";
+                comboBox1.Visible = false;
+                comboBox2.Visible = false;
+
+            }
+            if (checkBox1.Checked == false && checkBox2.Checked == false)
+            {
+
+                label4.Text=("Lütfen Bir Seçim Yapınız ! ");
+                comboBox1.Visible = false;
+                comboBox2.Visible = false;
+
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.BackColor == Color.Red)
+            {
+                MessageBox.Show("Lütfen Cihazınızı FastBoot Mode Alarak Bağlayınız", "Uyarı");
+            }
+            else
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    Process process = new Process();
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    startInfo.CreateNoWindow = true;
+                    startInfo.UseShellExecute = false;
+                    startInfo.RedirectStandardOutput = true;
+                    startInfo.FileName = "fastboot.exe";
+                    startInfo.Arguments = " flash boot boot.img";
+                    process.StartInfo = startInfo;
+                    process.Start();
+                }
+                if (comboBox1.SelectedIndex == 1)
+                {
+                    Process process = new Process();
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    startInfo.CreateNoWindow = true;
+                    startInfo.UseShellExecute = false;
+                    startInfo.RedirectStandardOutput = true;
+                    startInfo.FileName = "fastboot.exe";
+                    startInfo.Arguments = " flash recovery recovery.img";
+                    process.StartInfo = startInfo;
+                    process.Start();
+                }
+                if (comboBox1.SelectedIndex == 2)
+                {
+                    Process process = new Process();
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    startInfo.CreateNoWindow = true;
+                    startInfo.UseShellExecute = false;
+                    startInfo.RedirectStandardOutput = true;
+                    startInfo.FileName = "fastboot.exe";
+                    startInfo.Arguments = " flash logo logo.bin;";
+                    process.StartInfo = startInfo;
+                    process.Start();
+                }
+                if (comboBox2.SelectedIndex == 0)
+                {
+                    Process process = new Process();
+                    Process process2 = new Process();
+                    Process process3 = new Process();
+                    Process process4 = new Process();
+                    Process process5 = new Process();
+                    Process process6 = new Process();
+                    Process process7 = new Process();
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    ProcessStartInfo startInfo2 = new ProcessStartInfo();
+                    ProcessStartInfo startInfo3 = new ProcessStartInfo();
+                    ProcessStartInfo startInfo4 = new ProcessStartInfo();
+                    ProcessStartInfo startInfo5 = new ProcessStartInfo();
+                    ProcessStartInfo startInfo6 = new ProcessStartInfo();
+                    ProcessStartInfo startInfo7 = new ProcessStartInfo();
+                    startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    startInfo2.WindowStyle = ProcessWindowStyle.Hidden;
+                    startInfo3.WindowStyle = ProcessWindowStyle.Hidden;
+                    startInfo4.WindowStyle = ProcessWindowStyle.Hidden;
+                    startInfo5.WindowStyle = ProcessWindowStyle.Hidden;
+                    startInfo6.WindowStyle = ProcessWindowStyle.Hidden;
+                    startInfo7.WindowStyle = ProcessWindowStyle.Hidden;
+                    startInfo.CreateNoWindow = true;
+                    startInfo2.CreateNoWindow = true;
+                    startInfo3.CreateNoWindow = true;
+                    startInfo4.CreateNoWindow = true;
+                    startInfo5.CreateNoWindow = true;
+                    startInfo6.CreateNoWindow = true;
+                    startInfo7.CreateNoWindow = true;
+                    startInfo.UseShellExecute = false;
+                    startInfo2.UseShellExecute = false;
+                    startInfo3.UseShellExecute = false;
+                    startInfo4.UseShellExecute = false;
+                    startInfo5.UseShellExecute = false;
+                    startInfo6.UseShellExecute = false;
+                    startInfo7.UseShellExecute = false;
+                    startInfo.RedirectStandardOutput = true;
+                    startInfo2.RedirectStandardOutput = true;
+                    startInfo3.RedirectStandardOutput = true;
+                    startInfo4.RedirectStandardOutput = true;
+                    startInfo5.RedirectStandardOutput = true;
+                    startInfo6.RedirectStandardOutput = true;
+                    startInfo7.RedirectStandardOutput = true;
+                    startInfo.FileName = "fastboot.exe";
+                    startInfo2.FileName = "fastboot.exe";
+                    startInfo3.FileName = "fastboot.exe";
+                    startInfo4.FileName = "fastboot.exe";
+                    startInfo5.FileName = "fastboot.exe";
+                    startInfo6.FileName = "fastboot.exe";
+                    startInfo7.FileName = "fastboot.exe";
+
+                    startInfo.Arguments = " flash system teksim.img ";
+                    startInfo2.Arguments = " flash boot boot.img ";
+                    startInfo3.Arguments = " flash cache cache.img";
+                    startInfo4.Arguments = " flash userdata userdata.img";
+                    startInfo5.Arguments = " flash logo logo.bin";
+                    startInfo6.Arguments = " flash trustzone trustzone.img";
+                    startInfo7.Arguments = " flash uboot uboot.img";
+
+                    process.StartInfo = startInfo;
+                    process2.StartInfo = startInfo2;
+                    process3.StartInfo = startInfo3;
+                    process4.StartInfo = startInfo4;
+                    process5.StartInfo = startInfo5;
+                    process6.StartInfo = startInfo6;
+                    process7.StartInfo = startInfo7;
+
+                    process.Start();
+                    process2.Start();
+                    process3.Start();
+                    process4.Start();
+                    process5.Start();
+                    process6.Start();
+                    process7.Start();
+
+
+                }
+                if (comboBox2.SelectedIndex == 1)
+                {
+                    Process process = new Process();
+                    Process process2 = new Process();
+                    Process process3 = new Process();
+                    Process process4 = new Process();
+                    Process process5 = new Process();
+                    Process process6 = new Process();
+                    Process process7 = new Process();
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    ProcessStartInfo startInfo2 = new ProcessStartInfo();
+                    ProcessStartInfo startInfo3 = new ProcessStartInfo();
+                    ProcessStartInfo startInfo4 = new ProcessStartInfo();
+                    ProcessStartInfo startInfo5 = new ProcessStartInfo();
+                    ProcessStartInfo startInfo6 = new ProcessStartInfo();
+                    ProcessStartInfo startInfo7 = new ProcessStartInfo();
+                    startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    startInfo2.WindowStyle = ProcessWindowStyle.Hidden;
+                    startInfo3.WindowStyle = ProcessWindowStyle.Hidden;
+                    startInfo4.WindowStyle = ProcessWindowStyle.Hidden;
+                    startInfo5.WindowStyle = ProcessWindowStyle.Hidden;
+                    startInfo6.WindowStyle = ProcessWindowStyle.Hidden;
+                    startInfo7.WindowStyle = ProcessWindowStyle.Hidden;
+                    startInfo.CreateNoWindow = true;
+                    startInfo2.CreateNoWindow = true;
+                    startInfo3.CreateNoWindow = true;
+                    startInfo4.CreateNoWindow = true;
+                    startInfo5.CreateNoWindow = true;
+                    startInfo6.CreateNoWindow = true;
+                    startInfo7.CreateNoWindow = true;
+                    startInfo.UseShellExecute = false;
+                    startInfo2.UseShellExecute = false;
+                    startInfo3.UseShellExecute = false;
+                    startInfo4.UseShellExecute = false;
+                    startInfo5.UseShellExecute = false;
+                    startInfo6.UseShellExecute = false;
+                    startInfo7.UseShellExecute = false;
+                    startInfo.RedirectStandardOutput = true;
+                    startInfo2.RedirectStandardOutput = true;
+                    startInfo3.RedirectStandardOutput = true;
+                    startInfo4.RedirectStandardOutput = true;
+                    startInfo5.RedirectStandardOutput = true;
+                    startInfo6.RedirectStandardOutput = true;
+                    startInfo7.RedirectStandardOutput = true;
+                    startInfo.FileName = "fastboot.exe";
+                    startInfo2.FileName = "fastboot.exe";
+                    startInfo3.FileName = "fastboot.exe";
+                    startInfo4.FileName = "fastboot.exe";
+                    startInfo5.FileName = "fastboot.exe";
+                    startInfo6.FileName = "fastboot.exe";
+                    startInfo7.FileName = "fastboot.exe";
+
+                    startInfo.Arguments = " flash system ciftsim.img ";
+                    startInfo2.Arguments = " flash boot boot.img ";
+                    startInfo3.Arguments = " flash cache cache.img";
+                    startInfo4.Arguments = " flash userdata userdata.img";
+                    startInfo5.Arguments = " flash logo logo.bin";
+                    startInfo6.Arguments = " flash trustzone trustzone.img";
+                    startInfo7.Arguments = " flash uboot uboot.img";
+
+                    process.StartInfo = startInfo;
+                    process2.StartInfo = startInfo2;
+                    process3.StartInfo = startInfo3;
+                    process4.StartInfo = startInfo4;
+                    process5.StartInfo = startInfo5;
+                    process6.StartInfo = startInfo6;
+                    process7.StartInfo = startInfo7;
+
+                    process.Start();
+                    process2.Start();
+                    process3.Start();
+                    process4.Start();
+                    process5.Start();
+                    process6.Start();
+                    process7.Start();
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick_2(object sender, EventArgs e)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.FileName = "fastboot.exe";
+            startInfo.Arguments = " devices";
+            process.StartInfo = startInfo;
+            process.Start();
+            cihaz.Text = process.StandardOutput.ReadToEnd();
+            if (cihaz.Text == process.StandardOutput.ReadToEnd())
+            {
+                pictureBox1.BackColor = Color.Red;
+            }
+            else
+            {
+                pictureBox1.BackColor = Color.Green;
+            }
+            if (pictureBox1.BackColor == Color.Red)
+            {
+                cihaz.Text = ("Cihaz Bağlı Değildir.");
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://gsmturkey.net");
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
